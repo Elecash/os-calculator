fb10c39b033b7a2fada4e5a38c6fe956415c4c46
Raul Jimenez <elecash@gmail.com>
2020-08-15 17:34:47 +0200

    More accurate value calculation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class HomeComponent implements OnInit, OnDestroy {
     jsonTextArea = new FormControl('');
+    priceHour = new FormControl(75);
     starsCount = 0;
     totalHours = 0;
     totalValue = 0;
@@ -20,7 +21,8 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.subscriptions.push(
-            this.jsonTextArea.valueChanges.subscribe(v => this.parsePackage(v))
+            this.jsonTextArea.valueChanges.subscribe(value => this.parsePackage(value)),
+            this.priceHour.valueChanges.subscribe(value => this.calculateValue())
         );
     }
 
@@ -30,14 +32,44 @@ export class HomeComponent implements OnInit, OnDestroy {
         const devDependencies = Object.keys(jsonObj.devDependencies || {});
         const allDependencies = dependencies.concat(devDependencies);
 
-        console.log(allDependencies);
-
         this.http.post('https://api.npms.io/v2/package/mget', allDependencies).subscribe((resp: any[]) => {
-            const arr = Object.values(resp).filter(repo => repo.collected && repo.collected.github);
-            this.starsCount = arr.reduce((p, n) => n.collected.github.starsCount + p, 0);
-            this.totalHours = this.starsCount / 100;
-            this.totalValue = this.totalHours * 75;
+            this.parseResponse(resp);
         });
+    }
+
+    parseResponse(resp: any[]) {
+        const responseArray = Object.values(resp).filter(repo => repo.collected && repo.collected.github);
+        const pkgs = {};
+
+        responseArray.forEach(pkg => {
+            const organization = pkg.collected.metadata.name.split('/')[0];
+            if (!pkgs[organization]) {
+                pkgs[organization] = {
+                    organization: organization,
+                    starsCount: pkg.collected.github.starsCount,
+                    score: pkg.score.final,
+                    packages: [ pkg.collected.metadata.name ]
+                };
+            } else {
+                pkgs[organization].packages.push(pkg.collected.metadata.name);
+                pkgs[organization].score = pkgs[organization].score + pkg.score.final;
+                pkgs[organization].starsCount = pkgs[organization].starsCount > pkg.collected.github.starsCount ?
+                    pkgs[organization].starsCount :
+                    pkg.collected.github.starsCount;
+            }
+        });
+
+        const pkgsArray: any[] = Object.values(pkgs);
+
+        this.starsCount = Math.round(pkgsArray.reduce((p, n) => (n.starsCount / n.packages.length) + p, 0));
+        this.totalHours = Math.round(pkgsArray.reduce((p, n) => (n.starsCount * (n.score / n.packages.length) / 100) + p, 0));
+        this.calculateValue();
+    }
+
+    calculateValue() {
+        if (this.totalHours) {
+            this.totalValue = Math.round(this.totalHours * this.priceHour.value);
+        }
     }
 
     ngOnDestroy() {
